314aa864bd24a4820cdd29796a511ac6f937e9c3
Roxxum <adam@roxxum.tech>
2021-11-15 23:17:27 +1000

    Added infinite scrolling road

--- src/game.ts
+++ src/game.ts
@@ -2,6 +2,8 @@ import 'phaser'
 
 class Title extends Phaser.Scene
 {
+    title;
+
     constructor ()
     {
         super({
@@ -12,12 +14,12 @@ class Title extends Phaser.Scene
 
     preload ()
     {
-
+        this.load.image('title', 'assets/title.png');
     }
 
     create ()
     {
-
+        this.title = this.add.image(game.canvas.width * 0.5, game.canvas.height * 0.50, 'title');
     }
 
     update ()
@@ -31,6 +33,7 @@ class Title extends Phaser.Scene
 class Game extends Phaser.Scene
 {
     player;
+    roads = [];
 
     constructor ()
     {
@@ -43,21 +46,41 @@ class Game extends Phaser.Scene
     preload ()
     {
         this.load.image('player', 'assets/player.png');
+        this.load.image('road', 'assets/road.png');
     }
 
     create ()
     {
-        this.player = this.add.image(game.canvas.width * 0.5, game.canvas.height * 0.75, 'player');
+        var width = game.canvas.width;
+        var height = game.canvas.height;
+        var scale = width / 240 | 0;
+
+        for(let i = 0; i < scale; i++){
+            this.roads.push(this.add.image(240 * i, 0, 'road').setOrigin(0, 0));
+            this.roads.push(this.add.image(240 * i, -960, 'road').setOrigin(0, 0))
+        }
+
+        this.player = this.add.image(width * 0.5, height * 0.75, 'player');
     }
 
     update ()
     {
-        if (this.input.activePointer.isDown){
-            if (this.input.activePointer.x > this.player.x){
-                this.player.x +=5;
+        for(let i = 0; i < this.roads.length; i++){
+            if(this.roads[i].y > game.canvas.height){
+                this.roads[i].y = -960;
             }
-            else if (this.input.activePointer.x < this.player.x){
-                this.player.x -=5;
+            else{
+                this.roads[i].y += 1;    
+            }
+
+        }
+
+        if(this.input.activePointer.isDown){
+            if(this.input.activePointer.x > this.player.x){
+                this.player.x += 5;
+            }
+            else if(this.input.activePointer.x < this.player.x){
+                this.player.x -= 5;
             }
         }
     }
