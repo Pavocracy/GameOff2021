2c5d9ffa85a1e78cfba528da5d6d8c9137e9aef5
Roxxum <adam@roxxum.tech>
2021-11-22 20:06:32 +1000

    Added side of road and more scaling

--- src/game.ts
+++ src/game.ts
@@ -20,7 +20,7 @@ class Title extends Phaser.Scene
 
     create ()
     {
-        this.title = this.add.image(game.canvas.width * 0.5, game.canvas.height * 0.50, 'title');
+        this.title = this.add.image(game.canvas.width * 0.5, game.canvas.height * 0.5, 'title');
     }
 
     update ()
@@ -37,6 +37,7 @@ class Game extends Phaser.Scene
     roads = [];
     gameWidth = game.canvas.width;
     gameHeight = game.canvas.height;
+    roadScaledHeight = 0;
 
     constructor ()
     {
@@ -49,7 +50,9 @@ class Game extends Phaser.Scene
     preload ()
     {
         this.load.image('player', 'assets/player.png');
+        this.load.image('car', 'assets/car.png');
         this.load.image('road', 'assets/road.png');
+        this.load.image('road-side', 'assets/road-side.png');
     }
 
     create ()
@@ -61,22 +64,37 @@ class Game extends Phaser.Scene
         var heightScale = 1;
 
         if(this.gameHeight > roadHeight){
-            heightScale = this.gameHeight / roadHeight;
-        };
-        
-        for(let i = 0; i < tiles; i++){
-            this.roads.push(this.add.image((roadWidth * widthScale) * i, 0, 'road').setScale(widthScale, heightScale).setOrigin(0, 0));
-            this.roads.push(this.add.image((roadWidth * widthScale) * i, -roadHeight, 'road').setScale(widthScale, heightScale).setOrigin(0, 0))
+            heightScale = (this.gameHeight / roadHeight) * 1.01;
         };
 
-        this.player = this.add.image(this.gameWidth * 0.5, this.gameHeight * 0.75, 'player');
+        this.roadScaledHeight = roadHeight * heightScale
+
+        for(let i = 0; i < tiles; i++){
+            if(i == 0){
+                this.roads.push(this.add.image((roadWidth * widthScale) * i, 0, 'road-side').setScale(widthScale, heightScale).setOrigin(0, 0));
+                this.roads.push(this.add.image((roadWidth * widthScale) * i, -(roadHeight * heightScale), 'road-side').setScale(widthScale, heightScale).setOrigin(0, 0))
+            }
+            else if(i == Math.floor(tiles)-1){
+                this.roads.push(this.add.image((roadWidth * widthScale) * i, 0, 'road-side').setScale(widthScale, heightScale).setFlipX(true).setOrigin(0, 0));
+                this.roads.push(this.add.image((roadWidth * widthScale) * i, -(roadHeight * heightScale), 'road-side').setScale(widthScale, heightScale).setFlipX(true).setOrigin(0, 0))
+            }
+            else{
+                this.roads.push(this.add.image((roadWidth * widthScale) * i, 0, 'road').setScale(widthScale, heightScale).setOrigin(0, 0));
+                this.roads.push(this.add.image((roadWidth * widthScale) * i, -(roadHeight * heightScale), 'road').setScale(widthScale, heightScale).setOrigin(0, 0))
+            }
+        };
+
+        var playerWidth = this.textures.get("player").getSourceImage().width;
+        var playerHeight = this.textures.get("player").getSourceImage().height;
+        var playerScale = (roadWidth / 2) / playerWidth;
+        this.player = this.add.image(this.gameWidth * 0.5, this.gameHeight - ((playerHeight * playerScale) / 2), 'player').setScale(playerScale, playerScale);
     }
 
     update ()
     {
         for(let i = 0; i < this.roads.length; i++){
             if(this.roads[i].y > this.gameHeight){
-                this.roads[i].y = -this.roads[i].height;
+                this.roads[i].y = -this.roadScaledHeight;
             }
             else{
                 this.roads[i].y += 1;    
@@ -85,10 +103,10 @@ class Game extends Phaser.Scene
         };
 
         if(this.input.activePointer.isDown){
-            if(this.input.activePointer.x > this.player.x){
+            if(this.input.activePointer.x > this.player.x + 25){
                 this.player.x += 5;
             }
-            else if(this.input.activePointer.x < this.player.x){
+            else if(this.input.activePointer.x < this.player.x - 25){
                 this.player.x -= 5;
             }
         };
